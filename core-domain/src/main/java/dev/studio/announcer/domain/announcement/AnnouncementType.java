9a8ba551974f17924786c6b95a02a3a072967f40
LavenderEdit <gercermagden@gmail.com>
2026-06-10 11:17:07 -0500

    feat(core-domain): add AnnouncementType enum for classification

---
+++ core-domain/src/main/java/dev/studio/announcer/domain/announcement/AnnouncementType.java
@@ -0,0 +1,15 @@
+package dev.studio.announcer.domain.announcement;
+
+public enum AnnouncementType {
+    GLOBAL,
+    SCHEDULED,
+    EVENT_JOIN,
+    EVENT_QUIT,
+    EVENT_DEATH,
+    EVENT_WORLD_CHANGE,
+    EVENT_COMMAND,
+    NETWORK,
+    DISCORD,
+    PERMISSION_BASED,
+    RANK_BASED
+}
