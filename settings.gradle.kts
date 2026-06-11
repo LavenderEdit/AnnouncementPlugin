263966980f6b72482045ed69966d8cfab0eebbf5
LavenderEdit <gercermagden@gmail.com>
2026-06-11 11:11:29 -0500

    chore(build): add custom Maven repository for additional dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
         maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
         maven("https://repo.xenondevs.xyz/releases/")
+        maven("https://repo.loohpjames.com/repository/")
     }
 }
 
