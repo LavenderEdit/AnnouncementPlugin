5fc47ede7a1f125f38a353df1721b880858027c0
LavenderEdit <gercermagden@gmail.com>
2026-06-10 19:56:19 -0500

    build(platform-common): add Configurate YAML dependency
    - Include `org.spongepowered:configurate-yaml:4.2.0` for YAML-based configuration support.

--- platform-common/build.gradle.kts
+++ platform-common/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     api("net.kyori:adventure-api:4.17.0")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
+    implementation("org.spongepowered:configurate-yaml:4.2.0")
 }
