8ba79ecc4f195e308b18decb9a159cc5af33a595
LavenderEdit <gercermagden@gmail.com>
2026-06-11 11:08:48 -0500

    build(spigot): add DiscordSRV 1.28.0 as compileOnly dependency

--- platform-spigot/build.gradle.kts
+++ platform-spigot/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("net.kyori:adventure-text-serializer-gson:4.17.0")
     compileOnly("xyz.xenondevs.invui:invui:1.49")
     compileOnly("io.github.rysefoxx.anvilgui:anvilgui:1.6.5.4")
+    compileOnly("com.discordsrv:discordsrv:1.28.0")
 
     testImplementation("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 }
